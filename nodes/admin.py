9298f07599b507baf5e4b99690ba9ddfc77b3a8d
Roman <hmrfins@gmail.com>
2022-05-28 02:56:29 +0300

    base pipeline things

--- nodes/admin.py
+++ nodes/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 
-from nodes.models import Node
+from nodes.models import Node, Filter, FilterNode, Microservice
+
+
+class MicroserviceAdmin(admin.ModelAdmin):
+    list_fields = ('url',)
 
 
 class NodeAdmin(admin.ModelAdmin):
-    list_fields = ('name', 'url')
+    list_fields = ('base_url', 'name')
 
 
+class FilterAdmin(admin.ModelAdmin):
+    list_fields = ('name', 'description')
+    # todo display nodes
+
+
+class FilterNodeAdmin(admin.ModelAdmin):
+    list_select_related = ('filter', 'node_value', 'next_filter_node')
+
+
+admin.site.register(Microservice, MicroserviceAdmin)
 admin.site.register(Node, NodeAdmin)
+admin.site.register(Filter, FilterAdmin)
+admin.site.register(FilterNode, FilterNodeAdmin)
+
--- nodes/models.py
+++ nodes/models.py
@@ -1,7 +1,36 @@
+from urllib.parse import urljoin
+
 from django.db import models
+
+
+class Microservice(models.Model):
+    url = models.URLField(unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.url
 
 
 class Node(models.Model):
     url = models.URLField(unique=True)
-    name = models.TextField(unique=True)
+    name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    microservice = models.ForeignKey(Microservice, on_delete=models.SET_NULL, blank=True, null=True)
+
+    @property
+    def base_url(self):
+        if self.microservice:
+            return urljoin(self.microservice.url, self.url)
+        return self.url
+
+
+class Filter(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    # todo iterator
+
+
+class FilterNode(models.Model):
+    filter = models.ForeignKey(Filter, on_delete=models.CASCADE)
+    node_value = models.ForeignKey(Node, on_delete=models.CASCADE)
+    next_filter_node = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
